e18a5a899010a9ff230cba11ec54a5950915f2f8
Justin <90155396+JustinDMS@users.noreply.github.com>
2025-06-18 16:35:15 -0500

    Some inventory adjustments
    Doesn't include "extra" items that don't have icons (arm cannon, long beam, infinite bomb propulsion, wall jump boots)

--- scripts/game/am2r.gd
+++ scripts/game/am2r.gd
@@ -28,54 +28,59 @@ func _init(rdv_header : Dictionary) -> void:
 	# TODO: Fix inventory
 	var _all := InventoryInterface.AllButton.new(self)
 	var none := InventoryInterface.NoneButton.new(self)
-	var arm_cannon := InventoryInterface.PickupButton.new(self, &"Arm Cannon", &"res://data/games/am2r/item_images/dna.png")
-	var bombs := InventoryInterface.PickupButton.new(self, &"Bombs", &"res://data/games/am2r/item_images/morph_ball_bomb.png")
-	var power_grip := InventoryInterface.PickupButton.new(self, &"Power Grip", &"res://data/games/am2r/item_images/power_grip.png")
+	var missile_launcher := InventoryInterface.PickupButton.new(self, &"Missile Launcher", &"res://data/games/am2r/item_images/Missile Launcher.png")
+	var super_launcher := InventoryInterface.PickupButton.new(self, &"Super Missile Launcher", &"res://data/games/am2r/item_images/Super Missile Launcher.png")
+	var pb_launcher := InventoryInterface.PickupButton.new(self, &"Power Bomb Launcher", &"res://data/games/am2r/item_images/Power Bomb Launcher.png")
 	
-	var spider := InventoryInterface.PickupButton.new(self, &"Spider Ball", &"res://data/games/am2r/item_images/spider_ball.png")
-	var spring := InventoryInterface.PickupButton.new(self, &"Spring Ball", &"res://data/games/am2r/item_images/spring_ball.png")
-	var screw := InventoryInterface.PickupButton.new(self, &"Screw Attack", &"res://data/games/am2r/item_images/screw_attack.png")
-	var varia := InventoryInterface.PickupButton.new(self, &"Varia Suit", &"res://data/games/am2r/item_images/varia_suit.png")
-	var space := InventoryInterface.PickupButton.new(self, &"Space Jump", &"res://data/games/am2r/item_images/space_jump.png")
+	var charge := InventoryInterface.PickupButton.new(self, &"Charge Beam", &"res://data/games/am2r/item_images/Charge Beam.png")
+	var wave := InventoryInterface.PickupButton.new(self, &"Wave Beam", &"res://data/games/am2r/item_images/Wave Beam.png")
+	var spazer := InventoryInterface.PickupButton.new(self, &"Spazer Beam", &"res://data/games/am2r/item_images/Spazer Beam.png")
+	var plasma := InventoryInterface.PickupButton.new(self, &"Plasma Beam", &"res://data/games/am2r/item_images/Plasma Beam.png")
+	var ice := InventoryInterface.PickupButton.new(self, &"Ice Beam", &"res://data/games/am2r/item_images/Ice Beam.png")
 	
-	var speed := InventoryInterface.PickupButton.new(self, &"Speed Booster", &"res://data/games/am2r/item_images/speed_booster.png")
-	var hj := InventoryInterface.PickupButton.new(self, &"Hi-Jump", &"res://data/games/am2r/item_images/hi_jump_boots.png")
-	var grav := InventoryInterface.PickupButton.new(self, &"Gravity Suit", &"res://data/games/am2r/item_images/gravity_suit.png")
-	var charge := InventoryInterface.PickupButton.new(self, &"Charge Beam", &"res://data/games/am2r/item_images/charge_beam.png")
-	var ice := InventoryInterface.PickupButton.new(self, &"Ice Beam", &"res://data/games/am2r/item_images/ice_beam.png")
+	var morph := InventoryInterface.PickupButton.new(self, &"Morph Ball", &"res://data/games/am2r/item_images/Morph Ball.png")
+	var bombs := InventoryInterface.PickupButton.new(self, &"Bombs", &"res://data/games/am2r/item_images/Bombs.png")
+	var spider := InventoryInterface.PickupButton.new(self, &"Spider Ball", &"res://data/games/am2r/item_images/Spider Ball.png")
+	var spring := InventoryInterface.PickupButton.new(self, &"Spring Ball", &"res://data/games/am2r/item_images/Spring Ball.png")
+	var screw := InventoryInterface.PickupButton.new(self, &"Screw Attack", &"res://data/games/am2r/item_images/Screw Attack.png")
 	
-	var wave := InventoryInterface.PickupButton.new(self, &"Wave Beam", &"res://data/games/am2r/item_images/wave_beam.png")
-	var spazer := InventoryInterface.PickupButton.new(self, &"Spazer Beam", &"res://data/games/am2r/item_images/spazer_beam.png")
-	var plasma := InventoryInterface.PickupButton.new(self, &"Plasma Beam", &"res://data/games/am2r/item_images/plasma_beam.png")
-	var morph := InventoryInterface.PickupButton.new(self, &"Morph Ball", &"res://data/games/am2r/item_images/morph_ball.png")
-	var launcher := InventoryInterface.PickupButton.new(self, &"Missile Launcher", &"res://data/games/am2r/item_images/missile_launcher.png")
-	var super_launcher := InventoryInterface.PickupButton.new(self, &"Super Missile Launcher", &"res://data/games/am2r/item_images/sm_launcher.png")
-	var pb_launcher := InventoryInterface.PickupButton.new(self, &"Power Bomb Launcher", &"res://data/games/am2r/item_images/pb_launcher.png")
+	var power_grip := InventoryInterface.PickupButton.new(self, &"Power Grip", &"res://data/games/am2r/item_images/Power Grip.png")
+	var hi_jump := InventoryInterface.PickupButton.new(self, &"Hi-Jump", &"res://data/games/am2r/item_images/Hi-Jump Boots.png")
+	var space_jump := InventoryInterface.PickupButton.new(self, &"Space Jump", &"res://data/games/am2r/item_images/Space Jump.png")
+	var speed_boost := InventoryInterface.PickupButton.new(self, &"Speed Booster", &"res://data/games/am2r/item_images/Speed Booster.png")
+	var spacer := InventoryInterface.Spacer.new()
 	
-	var long := InventoryInterface.PickupButton.new(self, &"Long Beam", &"res://data/games/am2r/item_images/dna.png")
-	var ibp := InventoryInterface.PickupButton.new(self, &"Infinite Bomb Propulsion", &"res://data/games/am2r/item_images/dna.png")
-	var wjb := InventoryInterface.PickupButton.new(self, &"Walljump Boots", &"res://data/games/am2r/item_images/dna.png")
+	var varia := InventoryInterface.PickupButton.new(self, &"Varia Suit", &"res://data/games/am2r/item_images/Varia Suit.png")
+	var gravity := InventoryInterface.PickupButton.new(self, &"Gravity Suit", &"res://data/games/am2r/item_images/Gravity Suit.png")
+	var spacer2 := InventoryInterface.Spacer.new(0.74)
+	var dna_names : Array[StringName] = [&"Metroid DNA 1", &"Metroid DNA 2", &"Metroid DNA 3", &"Metroid DNA 4", &"Metroid DNA 5", &"Metroid DNA 6", &"Metroid DNA 7", &"Metroid DNA 8", &"Metroid DNA 9", &"Metroid DNA 10", &"Metroid DNA 11", &"Metroid DNA 12", &"Metroid DNA 13", &"Metroid DNA 14", &"Metroid DNA 15", &"Metroid DNA 16", &"Metroid DNA 17", &"Metroid DNA 18", &"Metroid DNA 19", &"Metroid DNA 20", &"Metroid DNA 21", &"Metroid DNA 22", &"Metroid DNA 23", &"Metroid DNA 24", &"Metroid DNA 25", &"Metroid DNA 26", &"Metroid DNA 27", &"Metroid DNA 28", &"Metroid DNA 29", &"Metroid DNA 30", &"Metroid DNA 31", &"Metroid DNA 32", &"Metroid DNA 33", &"Metroid DNA 34", &"Metroid DNA 35", &"Metroid DNA 36", &"Metroid DNA 37", &"Metroid DNA 38", &"Metroid DNA 39", &"Metroid DNA 40", &"Metroid DNA 41", &"Metroid DNA 42", &"Metroid DNA 43", &"Metroid DNA 44", &"Metroid DNA 45", &"Metroid DNA 46"]
+	var dna := InventoryInterface.MultiPickupSlider.new(self, dna_names, &"res://data/games/am2r/item_images/dna.png")
+	var spacer3 := InventoryInterface.Spacer.new(0.74)
+	
 	var alpha := InventoryInterface.PickupButton.new(self, &"Alpha Metroid Lure", &"res://data/games/am2r/item_images/dna.png")
 	var gamma := InventoryInterface.PickupButton.new(self, &"Gamma Metroid Lure", &"res://data/games/am2r/item_images/dna.png")
 	var zeta := InventoryInterface.PickupButton.new(self, &"Zeta Metroid Lure", &"res://data/games/am2r/item_images/dna.png")
 	var omega := InventoryInterface.PickupButton.new(self, &"Omega Metroid Lure", &"res://data/games/am2r/item_images/dna.png")
 	
-	var missiles := InventoryInterface.PickupSlider.new(self, &"Missiles", &"res://data/games/am2r/item_images/missile.png", 5, 50)
-	var supers := InventoryInterface.PickupSlider.new(self, &"Super Missiles", &"res://data/games/am2r/item_images/super_missile.png", 1, 20)
-	var etanks := InventoryInterface.PickupSlider.new(self, &"Energy Tank", &"res://data/games/am2r/item_images/energy_tank.png", 1, 10)
-	var pbs := InventoryInterface.PickupSlider.new(self, &"Power Bombs", &"res://data/games/am2r/item_images/power_bomb.png", 1, 20)
+	var missiles := InventoryInterface.PickupSlider.new(self, &"Missiles", &"res://data/games/am2r/item_images/Missile Expansion.png", 5, 50)
+	var supers := InventoryInterface.PickupSlider.new(self, &"Super Missiles", &"res://data/games/am2r/item_images/Super Missile Tank.png", 1, 20)
+	var etanks := InventoryInterface.PickupSlider.new(self, &"Energy Tank", &"res://data/games/am2r/item_images/Energy Tank.png", 1, 10)
+	var pbs := InventoryInterface.PickupSlider.new(self, &"Power Bombs", &"res://data/games/am2r/item_images/Power Bomb Tank.png", 1, 20)
+	
 	
 	inventory_layout = [
-		[_all, none, wjb, hj, space],
-		[arm_cannon, launcher, power_grip, charge, super_launcher],
-		[long, wave, ice, spazer, plasma],
-		[morph, bombs, spring, spider, pb_launcher],
-		[varia, grav, speed, screw, ibp],
+		[_all, none, missile_launcher, super_launcher, pb_launcher],
+		[charge, wave, spazer, plasma, ice],
+		[morph, bombs, spider, spring, screw],
+		[power_grip, hi_jump, space_jump, speed_boost, spacer],
+		[varia, gravity, spacer2, dna, spacer3],
 		[alpha, gamma, zeta, omega],
-		[missiles, supers],
+		[missiles, supers], 
 		[etanks, pbs]
 	]
 
+func get_game_id() -> StringName:
+	return &"am2r"
 func new_room_data() -> AM2RRoomData:
 	return AM2RRoomData.new()
 func new_room() -> AM2RRoom:
