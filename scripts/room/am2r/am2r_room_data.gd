0db7ffd6cf798feca60a09f2e2e2cb8ce2def523
JustinDMS <90155396+JustinDMS@users.noreply.github.com>
2025-06-20 17:21:30 -0500

    AM2RRoomData cleanup

--- scripts/room/am2r/am2r_room_data.gd
+++ scripts/room/am2r/am2r_room_data.gd
@@ -1,33 +1,32 @@
 class_name AM2RRoomData extends RoomData
 
-@export var map_name: String
-@export var x_position: float
-@export var y_position: float
-@export var image_width: float
-@export var image_height: float
+const ROOM_WIDTH : int = 320
+const ROOM_HEIGHT : int = 240
+const ROOM_DIVISOR : int = 8
 
-# TODO - Are these constants?
-@export var ROOM_WIDTH : int = 320
-@export var ROOM_HEIGHT : int = 240
-@export var ROOM_DIVISOR : int = 8
+var map_name : String
+var x_position : float
+var y_position : float
+var image_width : int
+var image_height : int
 
+@warning_ignore_start("integer_division") # https://github.com/godotengine/godot/issues/42966
 func init(_game : Game, _region : StringName, _name : String, _data : Dictionary) -> void:
 	super(_game, _region, _name, _data)
 	
 	map_name = _data["extra"]["map_name"]
+	
 	texture = get_room_texture()
-	
-	var all_x = []
-	var all_y = []
-	for ele in _data["extra"]["minimap_data"]:
-		all_x.append(ele["x"])
-		all_y.append(ele["y"])
-	all_x.sort()
-	all_y.sort()
-	
-	@warning_ignore_start("integer_division")
 	image_width = texture.get_width() / ROOM_DIVISOR
 	image_height = texture.get_height() / ROOM_DIVISOR
+	
+	var all_x : Array[int] = []
+	var all_y : Array[int] = []
+	for ele in _data["extra"]["minimap_data"]:
+		all_x.append( int(ele.x) )
+		all_y.append( int(ele.y) )
+	all_x.sort()
+	all_y.sort()
 	
 	if len(all_x) > 0:
 		x_position = all_x[0] * (ROOM_WIDTH / ROOM_DIVISOR)
@@ -37,4 +36,4 @@ func init(_game : Game, _region : StringName, _name : String, _data : Dictionary
 		y_position = 10
 
 func get_room_texture() -> Texture2D:
-	return load("res://data/games/am2r/room_images/%s.png" % [map_name])
+	return load("res://data/games/am2r/room_images/%s.png" % map_name)
